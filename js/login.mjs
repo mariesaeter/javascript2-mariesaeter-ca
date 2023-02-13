5bfe92c7b31f171d90f2acad23ceba289049d847
mariesaeter <marie.saeter@gmail.com>
2023-02-14 00:15:54 +0100

    Partial functional login

--- js/login.mjs
+++ js/login.mjs
@@ -1,9 +1,18 @@
 import { api_Url_Base } from "./main.mjs";
 import { POST_Options } from "./main.mjs";
+// import { postData } from "./main.mjs";
 
 async function signInUser(url, data) {
   try {
-    const options = { POST_Options, body: JSON.stringify(data) };
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    console.log(options);
 
     const response = await fetch(url, options);
     console.log(response);
@@ -16,3 +25,15 @@ async function signInUser(url, data) {
     console.log(error);
   }
 }
+
+const btnLogIn = document.getElementById("login-btn");
+btnLogIn.onclick = (e) => {
+  e.preventDefault();
+  const user = {
+    email: document.getElementById("inputEmail").value,
+    password: document.getElementById("inputPassword").value,
+  };
+  console.log(user);
+
+  signInUser(`${api_Url_Base}/auth/login`, user);
+};
--- js/main.mjs
+++ js/main.mjs
@@ -5,5 +5,19 @@ export const POST_Options = {
   headers: {
     "Content-Type": "application/json",
   },
-  //   body: JSON.stringify(data),
 };
+
+// export async function postData(url, data) {
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(data),
+//   };
+//   const response = await fetch(url, options);
+//   console.log(response);
+//   const json = await response.json();
+//   console.log(json);
+//   return json;
+// }
