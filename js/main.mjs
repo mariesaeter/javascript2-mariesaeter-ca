16edc9c2e0be892f509c841e28ab24ce66f5767a
mariesaeter <marie.saeter@gmail.com>
2023-02-13 09:58:10 +0100

    worked on register js

---
+++ js/main.mjs
@@ -0,0 +1,9 @@
+export const api_Url_Base = "https://api.noroff.dev/api/v1/social";
+
+export const POST_Options = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  //   body: JSON.stringify(data),
+};
---
+++ js/signup.mjs
@@ -0,0 +1,54 @@
+import { api_Url_Base } from "./main.mjs";
+import { POST_Options } from "./main.mjs";
+
+const registerForm = document.querySelector(".register-form");
+registerForm.addEventListener("submit", registerUser);
+
+/**
+ *
+ * @param {string} url
+ * @param {object} data
+ * @returns
+ */
+async function registerUser(url, data) {
+  try {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    const response = await fetch(url, options);
+    console.log(response);
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const user = [
+  { name: document.getElementById("inputName").value },
+  { email: document.getElementById("inputEmail").value },
+  { password: document.getElementById("inputPassword").value },
+];
+
+console.log(user);
+
+registerUser(`${api_Url_Base}/auth/register`, user);
+
+let users = [];
+
+// const btnRegister = document.getElementById("register-btn");
+// btnRegister.onclick = (e) => {
+//   const user = [
+//     { name: document.getElementById("inputName").value },
+//     { email: document.getElementById("inputEmail").value },
+//     { password: document.getElementById("inputPassword").value },
+//   ];
+//   return user;
+//   //   users.push(user);
+//   //   localStorage.setItem("Users", JSON.stringify(users));
+// };
